1235044634acb64e571cc5b092729e03fee305d6
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-12-15 11:23:33 +0200

    Add Files

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,9 @@
--- SQL script that lists all bands with Glam rock as their main style,
--- ranked by their longevity
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-  ORDER BY `lifespan` DESC;
+-- List all bands with Glam rock as their main style
+
+SELECT band_name,
+CASE WHEN split is NULL THEN
+	(YEAR(CURDATE()) - formed) ELSE 
+	(split - formed) END AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style)
+ORDER BY lifespan DESC;
